a3b768d633630f820b46711492fb70b5f6340c0f
XTZ206 <ywx2006@outlook.com>
2025-03-04 19:41:32 +0800

    feat: example ncurses subwin usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <ncurses.h>
+#include <string>
 
 int main(void)
 {
     initscr();
 
-    printw("Hello, World!");
+    int win_line = 20;
+    int win_col = 50;
+
+    WINDOW *win = subwin(stdscr, win_line, win_col, (LINES - win_line) / 2, (COLS - win_col) / 2);
+
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    std::string string = "Hello, World!";
+    mvwprintw(win, win_line / 2, (win_col - string.length()) / 2, "%s", string.c_str());
     refresh();
     getch();
 
