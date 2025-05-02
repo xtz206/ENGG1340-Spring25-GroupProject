34989a268bbc77d9c8667d18e4a2c66c6a37577b
ZOT-217 <1943197939@qq.com>
2025-05-02 18:18:23 +0800

    docs: docstring for render.cpp

--- src/render.cpp
+++ src/render.cpp
@@ -11,16 +11,47 @@
 
 #define ALL_SIZE Size(LINES, COLS)
 
+/**
+* @brief Constructor for the Window class.
+*
+* @param win The parent window.
+* @param s The size of the window.
+* @param p The position of the window.
+*
+*/
+
 Window::Window(WINDOW *win, Size s, Position p) : size(s), pos(p)
 {
     window = subwin(win, s.h, s.w, p.y, p.x);
 }
+
+/**
+ * @brief Constructor for the Window class.
+ * 
+ * @param win The parent window.
+ * @param s The size of the window.
+ * @param p The position of the window.
+ * 
+ */
 
 Window::Window(Window &win, Size s, Position p) : size(s), pos(p)
 {
     window = subwin(win.window, s.h, s.w, p.y, p.x);
 }
 
+/**
+ * @brief Prints a character with specified attributes at a given position within the window.
+ * 
+ * This function places a character (`ch`) at the specified position (`p`) in the window,
+ * applying the given attributes (`attr`). If the position is outside the bounds of the
+ * window, the function returns without performing any action.
+ * 
+ * @param p The position (x, y) where the character should be printed. 
+ *          It is represented as a `Position` object.
+ * @param ch The character to be printed, represented as a `chtype`.
+ * @param attr The attributes to be applied to the character, represented as an `attr_t`.
+ *             Attributes can include color, bold, underline, etc.
+ */
 void Window::print(Position p, chtype ch, attr_t attr)
 {
     if (p.y >= size.h || p.x >= size.w)
@@ -32,6 +63,19 @@ void Window::print(Position p, chtype ch, attr_t attr)
     wattroff(window, attr);
 }
 
+/**
+ * @brief Prints a string with specified attributes at a given position within the window.
+ * 
+ * This function places a string (`s`) at the specified position (`p`) in the window,
+ * applying the given attributes (`attr`). If the position is outside the bounds of the
+ * window, the function returns without performing any action.
+ * 
+ * @param p The position (x, y) where the string should be printed. 
+ *          It is represented as a `Position` object.
+ * @param s The string to be printed, represented as a `const char*`.
+ * @param attr The attributes to be applied to the string, represented as an `attr_t`.
+ *             Attributes can include color, bold, underline, etc.
+ */
 void Window::print(Position p, const char *s, attr_t attr)
 {
     if (p.y >= size.h || p.x >= size.w)
@@ -43,6 +87,20 @@ void Window::print(Position p, const char *s, attr_t attr)
     wattroff(window, attr);
 }
 
+
+/**
+ * @brief Prints a line of spaces with the specified attributes in a window.
+ * 
+ * This function fills an entire line of the window with spaces, starting from
+ * the beginning of the line (column 0) and extending to the width of the window.
+ * The spaces are printed with the specified text attributes.
+ * 
+ * @param line The line number (row) in the window where the spaces will be printed.
+ *             If the line number is greater than or equal to the height of the window,
+ *             the function returns without performing any action.
+ * @param attr The text attributes to be applied to the spaces (e.g., color, bold).
+ *             These attributes are applied using `wattron` and removed using `wattroff`.
+ */
 void Window::print_spaces(int line, attr_t attr)
 {
     if (line >= size.h)
@@ -54,6 +112,18 @@ void Window::print_spaces(int line, attr_t attr)
     wattroff(window, attr);
 }
 
+/**
+ * @brief Prints a string aligned to the left at a specified line in the window.
+ * 
+ * This function displays a string `s` starting from the leftmost position of the specified
+ * line in the window. If the string exceeds the width of the window, it is truncated to fit.
+ * The specified attributes `attr` are applied to the string while rendering.
+ * 
+ * @param line The line number (0-based index) where the string will be printed.
+ *             If the line number exceeds the height of the window, the function returns without printing.
+ * @param s The string to be printed.
+ * @param attr The attributes to be applied to the string (e.g., color, bold, etc.).
+ */
 void Window::print_left(int line, const std::string &s, attr_t attr)
 {
     if (line >= size.h)
@@ -73,6 +143,20 @@ void Window::print_left(int line, const std::string &s, attr_t attr)
     wattroff(window, attr);
 }
 
+/**
+ * @brief Prints a string centered on a specified line within the window.
+ *
+ * This function centers a given string on a specified line of the window.
+ * If the string is longer than the window's width, it truncates the string
+ * to fit within the window. The text is displayed with the specified
+ * attributes.
+ *
+ * @param line The line number (0-based index) where the string will be printed.
+ *             If the line number exceeds the window's height, the function
+ *             returns without performing any action.
+ * @param s The string to be printed.
+ * @param attr The text attributes to be applied (e.g., bold, underline).
+ */
 void Window::print_center(int line, const std::string &s, attr_t attr)
 {
     if (line >= size.h)
@@ -92,6 +176,18 @@ void Window::print_center(int line, const std::string &s, attr_t attr)
     wattroff(window, attr);
 }
 
+/**
+ * @brief Prints a string aligned to the right on a specified line within the window.
+ * 
+ * This function displays a string on a specified line of the window, aligning it to the right.
+ * If the string is longer than the window's width, it will be truncated to fit.
+ * The specified attributes are applied to the text during rendering.
+ * 
+ * @param line The line number (0-based index) where the string will be printed.
+ *             If the line number exceeds the window's height, the function returns without action.
+ * @param s The string to be printed. If its length exceeds the window's width, it will be truncated.
+ * @param attr The text attributes to be applied (e.g., color, bold, etc.).
+ */
 void Window::print_right(int line, const std::string &s, attr_t attr)
 {
     if (line >= size.h)
@@ -112,11 +208,32 @@ void Window::print_right(int line, const std::string &s, attr_t attr)
     wattroff(window, attr);
 }
 
+/**
+ * @brief Displays a debug message on the screen at a specified line.
+ * 
+ * This function uses the `mvwprintw` function to print a string to the 
+ * standard screen (`stdscr`) at the specified line number. It is useful 
+ * for debugging purposes to display messages during program execution.
+ * 
+ * @param str The debug message to be displayed.
+ * @param line The line number on the screen where the message will be printed.
+ */
 void Renderer::debug(const std::string &str, int line)
 {
     mvwprintw(stdscr, line, 1, "%s", str.c_str());
 }
 
+
+/**
+ * @brief Constructs a BasicMenuRenderer object to render a menu with specified size.
+ * 
+ * @param m Reference to the Menu object to be rendered.
+ * @param s Size of the menu to be displayed.
+ * 
+ * This constructor initializes the renderer by calculating the position of the menu
+ * on the screen, creating a bordered window for the menu, and setting up an inner
+ * window for rendering menu items.
+ */
 BasicMenuRenderer::BasicMenuRenderer(Menu &m, Size s)
     : menu(m), size(s), pos((ALL_SIZE - s - Size(2, 2)) / 2),
       box_window(Window(stdscr, s + Size(2, 2), pos)),
@@ -124,6 +241,11 @@ BasicMenuRenderer::BasicMenuRenderer(Menu &m, Size s)
 {
 }
 
+/**
+ * @brief Initializes the basic menu renderer by clearing the screen, 
+ *        drawing the window margin, and displaying the menu title at the center.
+ */
+ 
 void BasicMenuRenderer::init(void)
 {
     erase();
@@ -131,11 +253,21 @@ void BasicMenuRenderer::init(void)
     box_window.print_center(0, menu.get_title());
 }
 
+/**
+ * @brief Renders the basic menu by refreshing the item window.
+ */
 void BasicMenuRenderer::render(void)
 {
     item_window.refresh();
 }
 
+/**
+ * @brief Renders a basic menu interface by drawing menu items on the screen.
+ * 
+ * This function clears the item window and iterates through the menu items.
+ * It highlights the item at the current cursor position and displays all
+ * items centered in the window.
+ */
 void BasicMenuRenderer::draw(void)
 {
     item_window.erase();
@@ -152,12 +284,27 @@ void BasicMenuRenderer::draw(void)
     }
 }
 
+/**
+ * @brief Constructs a TitleMenuRenderer object to render a title menu.
+ * 
+ * @param m Reference to the TitleMenu object to be rendered.
+ * @param s Size of the rendering area.
+ */
 TitleMenuRenderer::TitleMenuRenderer(TitleMenu &m, Size s)
     : menu(m), size(s), pos((ALL_SIZE - s) / 2),
       title_window(Window(stdscr, s, pos + Size(1, 1)))
 {
 }
 
+/**
+ * @brief Initializes the title menu renderer by clearing the screen and rendering
+ *        the menu items on the title window.
+ * 
+ * This function erases the current screen content, then iterates through the menu
+ * items to display them on the title window. All items except the last one are 
+ * printed left-aligned, while the last item is printed centered with a reverse 
+ * attribute for emphasis.
+ */
 void TitleMenuRenderer::init(void)
 {
     erase();
@@ -179,6 +326,13 @@ void TitleMenuRenderer::draw(void)
 {
 }
 
+/**
+ * @brief Renders the save menu interface.
+ * 
+ * This function clears the item window and iterates through the menu items to display them.
+ * The currently selected menu item is highlighted. A warning message is displayed at the 
+ * bottom of the window to inform the user about the consequences of saving on full.
+ */
 void SaveMenuRenderer::draw(void)
 {
     item_window.erase();
@@ -197,6 +351,14 @@ void SaveMenuRenderer::draw(void)
     item_window.print_center(size.h - 2, "SAVE ON FULL WILL OVERWRITE", COLOR_PAIR(3));
 }
 
+/**
+ * @brief Constructs an EndMenuRenderer object to render the end menu of the game.
+ * 
+ * @param g Reference to the Game object, representing the current game state.
+ * @param m Reference to the Menu object, representing the menu to be rendered.
+ * @param ds Size object representing the dimensions of the description window.
+ * @param is Size object representing the dimensions of the item window.
+ */
 EndMenuRenderer::EndMenuRenderer(Game &g, Menu &m, Size ds, Size is)
     : menu(m), game(g), desc_size(ds), item_size(is), pos((ALL_SIZE - Size(is.h + ds.h + 2, is.w + 2)) / 2),
       box_window(Window(stdscr, Size(is.h + ds.h + 3, is.w + 2), pos)),
@@ -205,6 +367,14 @@ EndMenuRenderer::EndMenuRenderer(Game &g, Menu &m, Size ds, Size is)
 {
 }
 
+/**
+ * @brief Initializes and renders the end menu screen with game results.
+ * 
+ * This function clears the screen, draws the menu layout, and displays
+ * the game results including win/lose status, score, casualty count, 
+ * and the number of turns taken. It formats the score and casualty 
+ * values based on their magnitude for better readability.
+ */
 void EndMenuRenderer::init(void)
 {
     erase();
@@ -273,6 +443,13 @@ void EndMenuRenderer::render(void)
     item_window.refresh();
 }
 
+/**
+ * @brief Renders the end menu by drawing menu items on the screen.
+ * 
+ * This function clears the item window and iterates through the menu items.
+ * It highlights the item at the current cursor position and displays all
+ * items centered in the window.
+ */
 void EndMenuRenderer::draw(void)
 {
     item_window.erase();
@@ -290,6 +467,16 @@ void EndMenuRenderer::draw(void)
     }
 }
 
+/**
+ * @brief Constructs a TutorialMenuRenderer object to render a tutorial menu.
+ * 
+ * @param m Reference to the TutorialMenu object to be rendered.
+ * @param ps Size of the page window within the menu.
+ * @param is Size of the item window within the menu.
+ * 
+ * Initializes the renderer's position, box window, page window, and item window
+ * based on the provided sizes and menu layout.
+ */
 TutorialMenuRenderer::TutorialMenuRenderer(TutorialMenu &m, Size ps, Size is)
     : menu(m), page_size(ps), item_size(is), pos((ALL_SIZE - Size(ps.h + is.h + 3, ps.w + 2)) / 2),
       box_window(stdscr, Size(ps.h + is.h + 3, ps.w + 2), pos),
@@ -298,6 +485,13 @@ TutorialMenuRenderer::TutorialMenuRenderer(TutorialMenu &m, Size ps, Size is)
 {
 }
 
+/**
+ * @brief Initializes the tutorial menu renderer by setting up the display.
+ * 
+ * This function clears the current screen, draws the margin and horizontal 
+ * line for the menu box, and places the title of the menu at the center 
+ * of the top row.
+ */
 void TutorialMenuRenderer::init(void)
 {
     erase();
@@ -314,6 +508,15 @@ void TutorialMenuRenderer::render(void)
     item_window.refresh();
 }
 
+/**
+ * @brief Renders the tutorial menu on the screen by drawing the page content
+ *        and menu items with appropriate formatting.
+ * 
+ * This function clears the page and item windows, then iterates through the
+ * menu's page content and items to display them. The currently selected menu
+ * item is highlighted using reverse video formatting. Additionally, it displays
+ * page information at the bottom of the item window.
+ */
 void TutorialMenuRenderer::draw(void)
 {
     page_window.erase();
@@ -339,6 +542,18 @@ void TutorialMenuRenderer::draw(void)
     item_window.print_center(item_size.h - 1, menu.get_page_info());
 }
 
+/**
+ * @brief Constructs a TechMenuRenderer object to render a technology menu.
+ * 
+ * @param m Reference to the TechMenu object to be rendered.
+ * @param is Size of the menu items to be displayed.
+ * @param ds Size of the description area for the menu.
+ * 
+ * This constructor initializes the renderer with the given menu, item size, 
+ * and description size. It calculates the position of the rendering area 
+ * and sets up the necessary windows for rendering the menu, items, and 
+ * descriptions within a bordered box.
+ */
 TechMenuRenderer::TechMenuRenderer(TechMenu &m, Size is, Size ds)
     : menu(m), item_size(is), desc_size(ds), pos((ALL_SIZE - Size(is.h + ds.h + 3, is.w + 2)) / 2),
       box_window(stdscr, Size(is.h + ds.h + 3, is.w + 2), pos),
@@ -347,6 +562,13 @@ TechMenuRenderer::TechMenuRenderer(TechMenu &m, Size is, Size ds)
 {
 }
 
+/**
+ * @brief Initializes the TechMenuRenderer by setting up the menu display.
+ * 
+ * This function clears the screen, draws the margin for the menu box, 
+ * centers and prints the menu title, and draws horizontal and vertical 
+ * lines to separate the menu items visually.
+ */
 void TechMenuRenderer::init(void)
 {
     erase();
@@ -363,6 +585,13 @@ void TechMenuRenderer::render(void)
     desc_window.refresh();
 }
 
+/**
+ * @brief Renders the technical menu interface by drawing the menu items and their descriptions.
+ * 
+ * This function clears the item and description windows, then iterates through the menu items
+ * to display them. The currently selected item is highlighted. Additionally, it displays the 
+ * description of the selected item in the description window.
+ */
 void TechMenuRenderer::draw(void)
 {
     item_window.erase();
@@ -387,6 +616,14 @@ void TechMenuRenderer::draw(void)
     }
 }
 
+/**
+ * @brief Constructs a GameRenderer object to manage and render the game interface.
+ * 
+ * @param g Reference to the Game object containing game state and logic.
+ * @param m Reference to the OperationMenu object for managing user operations.
+ * @param s Size object representing the dimensions of certain UI components.
+ * @param fs Vector of integers specifying the heights of various information windows.
+ */
 GameRenderer::GameRenderer(Game &g, OperationMenu &m, Size s, const std::vector<int> &fs)
     : game(g), menu(m), fields(fs),
       map_size(game.get_size()), info_size(map_size.h + s.h + 1, s.w),
@@ -405,6 +642,29 @@ GameRenderer::GameRenderer(Game &g, OperationMenu &m, Size s, const std::vector<
 {
 }
 
+/**
+ * @brief Initializes the game renderer by setting up the layout and drawing the UI components.
+ * 
+ * This function erases the current screen and draws the necessary UI elements, including margins, 
+ * horizontal and vertical lines, and various labeled sections for the game interface. It also 
+ * places specific characters at intersections and prints labels for different sections of the UI.
+ * 
+ * The layout is divided into the following sections:
+ * - Map
+ * - General Information
+ * - City & Missile
+ * - Technology & Research
+ * - Super Weapon
+ * - Operation and Feedback
+ * 
+ * The function uses the `box_window` object to draw the UI components and relies on the dimensions 
+ * provided by `map_size`, `info_size`, `operation_size`, and `fields` to determine the positions 
+ * of the elements.
+ * 
+ * @note The function assumes that the `fields` vector contains at least three elements, which 
+ *       represent the heights of the "City & Missile", "Technology & Research", and "Super Weapon" 
+ *       sections, respectively.
+ */
 void GameRenderer::init(void)
 {
     erase();
@@ -447,6 +707,20 @@ void GameRenderer::init(void)
     box_window.print(Position(map_size.h + 1, operation_size.w + 3), "Feedback");
 }
 
+/**
+ * @brief Renders the game interface by refreshing all the relevant windows.
+ * 
+ * This function updates the display of various game interface components,
+ * ensuring that all windows are redrawn with the latest information.
+ * It is responsible for refreshing the following windows:
+ * - Map window
+ * - General information window
+ * - Selected information window
+ * - Technology information window
+ * - Super weapon information window
+ * - Operation window
+ * - Feedback window
+ */
 void GameRenderer::render(void)
 {
     map_window.refresh();
@@ -458,6 +732,30 @@ void GameRenderer::render(void)
     feedback_window.refresh();
     // refresh();
 }
+
+/**
+ * @brief Renders the entire game interface, including map, general information, 
+ *        selected object information, technology tree, super weapon status, 
+ *        operation menu, and feedback messages.
+ * 
+ * This function is responsible for clearing and redrawing all game windows 
+ * to reflect the current state of the game. It handles the following:
+ * 
+ * - Map Window: Displays the game map, including terrain, missiles, and cursor.
+ * - General Info Window: Shows game statistics such as turn, deposit, productivity, 
+ *   enemy HP, and system statuses.
+ * - Selected Info Window: Displays detailed information about the currently 
+ *   selected missile or city.
+ * - Tech Info Window: Shows the current research status, available technologies, 
+ *   and researched technologies.
+ * - Super Weapon Info Window: Displays the status of super weapons, including 
+ *   readiness and remaining counters.
+ * - Operation Window: Displays the operation menu with selectable options.
+ * - Feedback Window: Shows recent feedback messages from the game.
+ * 
+ * The function ensures that all visual elements are updated to match the 
+ * current game state, providing a seamless user interface.
+ */
 
 void GameRenderer::draw(void)
 {
