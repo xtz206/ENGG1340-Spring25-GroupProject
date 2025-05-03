fb007707ff863e2a37c000373415682925a951c0
XTZ206 <ywx2006@outlook.com>
2025-05-03 14:15:47 +0800

    fix: remove redefinition of TitleVideo class

--- src/menu.h
+++ src/menu.h
@@ -109,22 +109,12 @@ public:
     TitleMenu(const std::vector<std::string> &t, const std::string &d);
 };
 
-class TitleVideo
-{
-private:
-    size_t index;
-    std::vector<std::vector<std::string>> frames;
-
-public:
-    TitleVideo(const std::vector<std::vector<std::string>> &fs) : index(0), frames(fs) {};
-    int get_index(void) const { return index; };
-    const std::vector<std::string> &get_frame(void) const { return frames.at(index); };
-    const std::vector<std::string> &get_frame(int i) const { return frames.at(i); };
-    void next_frame(void) { index = (index + 1) % frames.size(); };
-    void prev_frame(void) { index = (index - 1 + frames.size()) % frames.size(); };
-    bool is_end(void) const { return index == frames.size() - 1; };
-};
-
+/**
+ * @class TitleVideo
+ * @brief Game title screen video model
+ *
+ * Displays a sequence of frames for the title screen.
+ */
 class TitleVideo
 {
 private:
